1accd606fa84d853a04f094a50cd40accdd011b5
Aman Raj <amanraj04687@gmail.com>
2026-02-21 16:10:22 +0530

    Revert "minor change"
    This reverts commit 273c10028b04ae23c7fbd788d3b8b2632b77c8b2.

--- Services/src/db.ts
+++ Services/src/db.ts
@@ -17,14 +17,9 @@ function getDbName(): string {
   return 'foodops'
 }
 
-const MONGO_CONNECT_OPTIONS = {
-  serverSelectionTimeoutMS: 10_000,
-  connectTimeoutMS: 10_000,
-}
-
 export async function connectDb(): Promise<Db> {
   if (db) return db
-  client = new MongoClient(uri, MONGO_CONNECT_OPTIONS)
+  client = new MongoClient(uri)
   await client.connect()
   const dbName = getDbName()
   db = client.db(dbName)
